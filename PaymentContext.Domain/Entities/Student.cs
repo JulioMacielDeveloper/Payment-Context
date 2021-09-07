3c7163d64351806dba2c96e30a2ea25253af28f9
JulioMacielDeveloper <julio-developer@outlook.com>
2021-09-07 15:06:16 -0300

    Refactored Properties and methods

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -1,15 +1,18 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PaymentContext.Domain.Entities
 {
     public class Student
     {
+        private IList<Subscription> _subscription;
         public Student(string firstName, string lastName, string document, string email)
         {
             FirstName = firstName;
             LastName = lastName;
             Document = document;
             Email = email;
+            _subscription = new List<Subscription>();
         }
 
         public string FirstName { get; private set; }
@@ -17,6 +20,23 @@ namespace PaymentContext.Domain.Entities
         public string Document { get; private set; }
         public string Email { get; private set; }
         public string Address { get; private set; }
-        public List<Subscription> Subscriptions { get; set; }
+        public IReadOnlyCollection<Subscription> Subscriptions
+        {
+            get
+            {
+                return _subscription.ToArray();
+            }
+
+
+        }
+
+
+        public void AddSubscription(Subscription subscription)
+        {
+            foreach (var sub in Subscriptions)
+                sub.Inactivate();
+
+            _subscription.Add(subscription);
+        }
     }
 }
